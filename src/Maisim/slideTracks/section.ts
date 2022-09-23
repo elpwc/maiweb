f5188ec83f0f910a70d67bed9c4923e2f4448abd
elpwc/wniko <elpwc@hotmail.com>
2022-09-23 17:21:43 +0800

    Judge: SLIDE TRACK判定 <>

--- src/Maisim/slideTracks/section.ts
+++ src/Maisim/slideTracks/section.ts
@@ -67,39 +67,63 @@ export const section_A1 = (type: '-' | '^' | '<' | '>' | 'v' | 'p' | 'q' | 's' |
           res.push({ start: points[i] / ((endPos - 1) / 3), areas: areas[i] });
         }
       } else if (endPos > 5 && endPos <= 8) {
-        const points = [0, 0.104167, 0.416667, 0.729167, 1];
+        const points = [0, 0.104167, 0.4375, 0.770833, 1];
         const areas = [['A1'], ['D1', 'A8'], ['D8', 'A7'], ['D7', 'A6']];
         for (let i = 0; i < endPos - 4; i++) {
           res.push({ start: points[i] / ((9 - endPos) / 3), areas: areas[i] });
         }
       }
       return res;
-    case '<':  
-    const res2 = [];
-    if (startPos <= 2 || startPos >= 7) {
-      if (endPos > 1) {
-        return {
-        };
-      } else if (endPos === 1) {
-        return {
-        };
+    case '<':
+      const res2 = [];
+      if (startPos <= 2 || startPos >= 7) {
+        const points = [0, 0.0390625, 0.1640625, 0.2890625, 0.4140625, 0.5390625, 0.6640625, 0.7890625, 0.9140625, 1];
+        const areas = [['A1'], ['D1', 'A8'], ['D8', 'A7'], ['D7', 'A6'], ['D6', 'A5'], ['D5', 'A4'], ['D4', 'A3'], ['D3', 'A2'], ['D2', 'A1']];
+
+        if (endPos > 1) {
+          for (let i = 0; i < 10 - endPos; i++) {
+            res2.push({ start: points[i] / ((9 - endPos) / 8), areas: areas[i] });
+          }
+        } else if (endPos === 1) {
+          for (let i = 0; i < 9; i++) {
+            res2.push({ start: points[i] / 1, areas: areas[i] });
+          }
+        }
+      } else if (startPos >= 3 && startPos <= 6) {
+        const points = [0, 0.0390625, 0.1640625, 0.2890625, 0.4140625, 0.5390625, 0.6640625, 0.7890625, 0.9140625, 1];
+        const areas = [['A1'], ['D2', 'A2'], ['D3', 'A3'], ['D4', 'A4'], ['D5', 'A5'], ['D6', 'A6'], ['D7', 'A7'], ['D8', 'A8'], ['D1', 'A1']];
+
+        for (let i = 0; i < endPos; i++) {
+          res2.push({ start: points[i] / ((endPos - 1) / 8), areas: areas[i] });
+        }
       } else {
-        return { x: 0, y: 0, direction: 0 };
       }
-    } else if (startPos >= 3 && startPos <= 6) {
-      if (endPos > 1) {
-        return {
-        };
-      } else if (endPos === 1) {
-        return {
-        };
-      } else {
-      }
-    } else {
-    }
-      break;
+      return res2;
     case '>':
-      break;
+      const res3 = [];
+      if (startPos <= 2 || startPos >= 7) {
+        const points = [0, 0.0390625, 0.1640625, 0.2890625, 0.4140625, 0.5390625, 0.6640625, 0.7890625, 0.9140625, 1];
+        const areas = [['A1'], ['D2', 'A2'], ['D3', 'A3'], ['D4', 'A4'], ['D5', 'A5'], ['D6', 'A6'], ['D7', 'A7'], ['D8', 'A8'], ['D1', 'A1']];
+
+        if (endPos > 1) {
+          for (let i = 0; i < endPos; i++) {
+            res3.push({ start: points[i] / ((endPos - 1) / 8), areas: areas[i] });
+          }
+        } else if (endPos === 1) {
+          for (let i = 0; i < 9; i++) {
+            res3.push({ start: points[i] / 1, areas: areas[i] });
+          }
+        }
+      } else if (startPos >= 3 && startPos <= 6) {
+        const points = [0, 0.0390625, 0.1640625, 0.2890625, 0.4140625, 0.5390625, 0.6640625, 0.7890625, 0.9140625, 1];
+        const areas = [['A1'], ['D1', 'A8'], ['D8', 'A7'], ['D7', 'A6'], ['D6', 'A5'], ['D5', 'A4'], ['D4', 'A3'], ['D3', 'A2'], ['D2', 'A1']];
+
+        for (let i = 0; i < 10 - endPos; i++) {
+          res3.push({ start: points[i] / ((9 - endPos) / 8), areas: areas[i] });
+        }
+      } else {
+      }
+      return res3;
     case 'v':
       break;
     case 'p':
